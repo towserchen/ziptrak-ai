6a2309497314fad001fa0d4b9e0fdf7768041c1e
towser <towserchen@gmail.com>
2024-11-01 00:52:08 +1030

    Split app to app and api, adjust api incoming params

---
+++ api.py
@@ -0,0 +1,72 @@
+from fastapi import FastAPI, File, Form, UploadFile, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+import time
+import os
+
+import app as detect_app
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins='*',
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# 5m
+MAX_FILE_SIZE = 5 * 1024 * 1024
+
+
+@app.post("/detect")
+async def upload_image(
+    upload_file: UploadFile = File(...),
+    is_window_detected: int = Form(1),
+    save_processed_images: int = Form(1)
+):
+    print(is_window_detected)
+    print(save_processed_images)
+
+    if is_window_detected > 0:
+        is_window_detected = True
+    else:
+        is_window_detected = False
+
+    if save_processed_images > 0:
+        save_processed_images = True
+    else:
+        save_processed_images = False
+
+    print(is_window_detected)
+
+    if upload_file.content_type not in ["image/jpeg", "image/png"]:
+        raise HTTPException(status_code=400, detail="Invalid file type. Only JPG and PNG are allowed.")
+    
+    if upload_file.file.seek(0, os.SEEK_END) > MAX_FILE_SIZE:
+        raise HTTPException(status_code=400, detail="File size exceeds the limit of 5MB.")
+    upload_file.file.seek(0)
+
+    timestamp = int(time.time() * 1000)
+    file_extension = os.path.splitext(upload_file.filename)[1]
+    new_file_name = f"{timestamp}{file_extension}"
+
+    file_path = f"uploads/{new_file_name}"
+    file_location = file_path
+    with open(file_location, "wb") as file:
+        file.write(await upload_file.read())
+
+    result = detect_app.detect_file(file_path, is_window_detected, False, False)
+    coordinate_list = []
+
+    for coordinate in result:
+        _coordinate = [[int(x) for x in point] for point in coordinate]
+        coordinate_list.append(_coordinate)
+
+    print(coordinate_list)
+
+    return JSONResponse(content={
+        "coordinate_list": coordinate_list
+    })
--- app.py
+++ app.py
@@ -6,27 +6,8 @@ import detect
 from utils import tool
 import sam
 
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddleware
-
-import time
-import os
-
-# 5m
-MAX_FILE_SIZE = 5 * 1024 * 1024
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins='*',
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 def detect_file(file_path, is_window_detected=True, save_processed_images=True, show_final_image=True):
 
@@ -132,54 +113,7 @@ def detect_file(file_path, is_window_detected=True, save_processed_images=True, 
 
 
 if __name__ == '__main__':
-    file_path = 'samples/13.jpg'
+    file_path = 'samples/5.jpg'
     is_window_detected = False
 
-    #detect_file(file_path, is_window_detected, True, True)
-
-
-
-@app.post("/detect")
-async def upload_image(
-    upload_file: UploadFile = File(...),
-    is_window_detected: int = 1,
-    save_processed_images: int = 1
-):    
-    if is_window_detected > 0:
-        is_window_detected = True
-    else:
-        is_window_detected = False
-
-    if save_processed_images > 0:
-        save_processed_images = True
-    else:
-        save_processed_images = False
-
-    if upload_file.content_type not in ["image/jpeg", "image/png"]:
-        raise HTTPException(status_code=400, detail="Invalid file type. Only JPG and PNG are allowed.")
-    
-    if upload_file.file.seek(0, os.SEEK_END) > MAX_FILE_SIZE:
-        raise HTTPException(status_code=400, detail="File size exceeds the limit of 5MB.")
-    upload_file.file.seek(0)
-
-    timestamp = int(time.time() * 1000)
-    file_extension = os.path.splitext(upload_file.filename)[1]
-    new_file_name = f"{timestamp}{file_extension}"
-
-    file_path = f"uploads/{new_file_name}"
-    file_location = file_path
-    with open(file_location, "wb") as file:
-        file.write(await upload_file.read())
-
-    result = detect_file(file_path, is_window_detected, False, False)
-    coordinate_list = []
-
-    for coordinate in result:
-        _coordinate = [[int(x) for x in point] for point in coordinate]
-        coordinate_list.append(_coordinate)
-
-    print(coordinate_list)
-
-    return JSONResponse(content={
-        "coordinate_list": coordinate_list
-    })
+    detect_file(file_path, is_window_detected, True, True)
